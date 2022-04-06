4c3465c22290f10d7fb37f3093e1e6df2597c555
Robin Åstedt <robin.astedt@gmail.com>
2022-04-07 00:09:00 +0200

    Refactor first function

--- include/better_bitset.hpp
+++ include/better_bitset.hpp
@@ -38,6 +38,18 @@ namespace better_bitset
         /// @brief The storage type. Bits are stored from LSB to MSB
         /// and populate lower order storage positions first
         using Storage_t = std::array<Inner_t, NUM_CHUNKS>;
+
+        template<auto COUNT_FUNC>
+        constexpr size_t first_func_impl() const noexcept {
+            size_t pos = 0;
+            for (Inner_t chunk : m_storage) {
+                size_t chunk_pos = COUNT_FUNC(chunk);
+                pos += chunk_pos;
+                if (chunk_pos != sizeof(Inner_t) * 8)
+                    return pos;
+            }
+            return N;
+        }
     public:
         constexpr BitSet() noexcept : m_storage() {}
         /// @param storage The storage
@@ -62,7 +74,7 @@ namespace better_bitset
             // and that the last storage is the mask of the bit size
             return std::all_of(m_storage.begin(), std::prev(m_storage.end()),
                 [](Inner_t val) { return val == std::numeric_limits<Inner_t>::max(); }) &&
-                m_storage[NUM_CHUNKS - 1] == LAST_MASK;
+                m_storage.back() == LAST_MASK;
         }
         /// @return True if any of the bits are set to 1
         constexpr bool any() const noexcept
@@ -86,28 +98,12 @@ namespace better_bitset
         /// @return The position of the first one in the bitset
         constexpr size_t first_one() const noexcept
         {
-            size_t pos = 0;
-            for (size_t i = 0; i < NUM_CHUNKS; ++i)
-            {
-                size_t chunk_pos = std::countr_zero(m_storage[i]);
-                pos += chunk_pos;
-                if (chunk_pos != sizeof(Inner_t) * 8)
-                    return pos;
-            }
-            return N;
+            return first_func_impl<std::countr_zero<Inner_t>>();
         }
         /// @return The position of the first zero in the bitset
         constexpr size_t first_zero() const noexcept
         {
-            size_t pos = 0;
-            for (size_t i = 0; i < NUM_CHUNKS; ++i)
-            {
-                size_t chunk_pos = std::countr_one(m_storage[i]);
-                pos += chunk_pos;
-                if (chunk_pos != sizeof(Inner_t) * 8)
-                    return pos;
-            }
-            return N;
+            return first_func_impl<std::countr_one<Inner_t>>();
         }
         /// @brief Tests the bit at a an index. Does not perform a bounds
         /// check in release
